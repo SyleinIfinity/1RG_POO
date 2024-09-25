b47b5b0ab27f4dd47d6f8bac7e4a6dee56ceffa3
Sylein1 <khanhsky2k5nam@gmail.com>
2024-09-25 20:25:26 +0700

    KHOI TAO LIST

---
+++ BAITHUCHANH/TUAN10/test_9.java
@@ -0,0 +1,5 @@
+public class test_9 {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
---
+++ BAITHUCHANH/TUAN12/test_11.java
@@ -0,0 +1,5 @@
+public class test_11 {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
